d8d69be390d385c7a2a3249cf839f85fcd8f18b8
Santosh Singh <santosh102969@gmail.com>
2025-09-21 15:51:53 +0530

    Add

--- js/world.js
+++ js/world.js
@@ -1,11 +1,21 @@
 class World {
-   constructor(graph, roadWidth = 100, roadRoundness = 10) {
+   constructor(graph,
+      roadWidth = 100, 
+      roadRoundness = 10,
+      buildingWidth =150,
+      buildingMinLength =150,
+      spacing = 50
+   ) {
       this.graph = graph;
       this.roadWidth = roadWidth;
       this.roadRoundness = roadRoundness;
+      this.buildingWidth = buildingWidth;
+      this.buildingMinLength = buildingMinLength
+      this.spacing = spacing;
 
       this.envelopes = [];
       this.roadBorders = [];
+      this.building = []
 
       this.generate();
    }
@@ -19,8 +29,40 @@ class World {
       }
 
       this.roadBorders = Polygon.union(this.envelopes.map((e) => e.poly));
+
+      this.building = this.#generateBuildings();
+      
    }
 
+   #generateBuildings(){
+      const tmpEnvelope = []
+      for(const seg of this.graph.segments){
+         tmpEnvelope.push( new Envelope(
+            seg,
+            this.roadWidth + this.buildingWidth + this.spacing * 2,
+            this.roadRoundness
+         ));
+      } 
+
+      const gudies = Polygon.union(tmpEnvelope.map((e) => e.poly))
+
+      for(let i=0 ; i<gudies.length;i++){
+         const seg = gudies[i];
+         if(seg.length() < this.buildingMinLength){
+            gudies.splice(i,1);
+            i--;
+         }
+      }
+
+      const supports = []
+      for(let seg of gudies){
+         
+      }
+      
+      
+      return gudies;
+   }
+   
    draw(ctx) {
       for (const env of this.envelopes) {
          env.draw(ctx, { fill: "#BBB", stroke: "#BBB", lineWidth: 15 });
@@ -30,6 +72,9 @@ class World {
       }
       for (const seg of this.roadBorders) {
          seg.draw(ctx, { color: "white", width: 4 });
+      }
+      for(const bld of this.building){
+         bld.draw(ctx)   
       }
    }
 }
