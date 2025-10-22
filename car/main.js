177a1f73bc6a280a7349973b53a37328fc243029
Santosh Singh <santosh102969@gmail.com>
2025-10-23 02:26:38 +0530

    add

---
+++ car/main.js
@@ -0,0 +1,81 @@
+const carCanvas=document.getElementById("carCanvas");
+carCanvas.width=window.innerWidth - 330;
+const networkCanvas=document.getElementById("networkCanvas");
+networkCanvas.width=300;
+
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+
+
+const trafficCars = 10;
+
+const N=1;
+const cars=generateCars(N);
+let bestCar=cars[0];
+if(localStorage.getItem("bestBrain")){
+    for(let i=0;i<cars.length;i++){
+        cars[i].brain=JSON.parse(
+            localStorage.getItem("bestBrain"));
+        if(i!=0){
+            NeuralNetwork.mutate(cars[i].brain,0.1);
+        }
+    }
+}
+
+
+const traffic=[];
+const roadBorders = []
+
+function addCars(){
+    traffic = addNewPath(trafficCars,traffic)
+}
+
+animate();
+
+function save(){
+    localStorage.setItem("bestBrain",
+        JSON.stringify(bestCar.brain));
+}
+
+function discard(){
+    localStorage.removeItem("bestBrain");
+}
+
+function generateCars(N){
+    const cars=[];
+    for(let i=1;i<=N;i++){
+        cars.push(new Car(100,100,30,50,"KEYS"));
+    }
+    return cars;
+}
+
+function animate(time){
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].update(roadBorders,[]);
+    }
+    for(let i=0;i<cars.length;i++){
+        cars[i].update(roadBorders,traffic);
+    }
+    bestCar=cars.find(
+        c=>c.y==Math.min(
+            ...cars.map(c=>c.y)
+        ));
+
+    carCanvas.height=window.innerHeight;
+    networkCanvas.height=window.innerHeight;
+
+
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].draw(carCtx,"red");
+    }
+    carCtx.globalAlpha=0.2;
+    for(let i=0;i<cars.length;i++){
+        cars[i].draw(carCtx,"blue");
+    }
+    carCtx.globalAlpha=1;
+    bestCar.draw(carCtx,"blue",true);
+
+    networkCtx.lineDashOffset=-time/50;
+    Visualizer.drawNetwork(networkCtx,bestCar.brain);
+    requestAnimationFrame(animate);
+}
